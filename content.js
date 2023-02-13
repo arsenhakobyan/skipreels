4688977752109b75f816bb12dc4a967ad2cceb38
arsenhakobyan <hakobyan.ars@gmail.com>
2023-02-14 01:09:39 +0400

    Disable Facebook Reels as well.

--- content.js
+++ content.js
@@ -1,11 +1,28 @@
 chrome.storage.sync.get("minDuration", function(data) {
     var shelfElements = document.querySelectorAll("ytd-rich-shelf-renderer");
-    console.log("NumOfSpan: ", shelfElements.length)
     for (var i = 0; i < shelfElements.length; i++) {
         var shelfElement = shelfElements[i];
         if (shelfElement.hasAttribute("is-shorts")) {
             shelfElement.style.display = "none";
-            console.log("Setting display None for Shorts");
         }
-    }
+    };
+    var observer = new MutationObserver(function(mutations) {
+        var spanElements = document.getElementsByTagName("span");
+        for (var i = 0; i < spanElements.length; i++) {
+            var spanElement = spanElements[i];
+            if (spanElement.innerText.indexOf("Reels and short videos") !== -1) {
+                var parentElement = spanElement.parentNode;
+                for (var j = 0; j < 10; j++) {
+                    parentElement = parentElement.parentNode;
+                }
+                parentElement.style.display = "none";
+                break;
+            }
+        }
+    });
+
+    observer.observe(document, {
+        childList: true,
+        subtree: true
+    });
 });
